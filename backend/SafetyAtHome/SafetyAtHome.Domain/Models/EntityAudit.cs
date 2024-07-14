e84b55f57a4aed6317454e98ae70fde2ae88742a
Leyton M. Espitia Diaz <leyton_espitia@hotmail.com>
2024-07-14 18:20:38 -0500

    Agregar y aplicar campos de auditoría a las entidades, actualizar migraciones y modelo User

---
+++ backend/SafetyAtHome/SafetyAtHome.Domain/Models/EntityAudit.cs
@@ -0,0 +1,11 @@
+﻿namespace SafetyAtHome.Domain.Data.Models
+{
+    public abstract class EntityAudit : Entity
+    {
+        public bool IsDeleted { get; set; } = false;
+        public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+        public Guid CreatedBy { get; set; }
+        public DateTimeOffset? UpdatedAt { get; set; } = null!;
+        public Guid? UpdatedBy { get; set; } = null!;
+    }
+}
