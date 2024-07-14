00b2989e08bf07bb55b198fdb913db86322821bd
Leyton M. Espitia Diaz <leyton_espitia@hotmail.com>
2024-07-14 15:51:08 -0500

    Implementación de encriptación de contraseña y ajuste de campos en UserModel

--- backend/SafetyAtHome/SafetyAtHome.Common/Models/Auth/UserModel.cs
+++ backend/SafetyAtHome/SafetyAtHome.Common/Models/Auth/UserModel.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace SafetyAtHome.Common.Models.Auth
 {
     public class UserModel
     {
-        public Guid Id { get; set; }
+        public Guid? Id { get; set; } = Guid.NewGuid();
         public string Username { get; set; }
         public string Email { get; set; }
-        public DateTime CreatedAt { get; set; }
+        public string Password { get; set; }
+        public DateTime? CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
